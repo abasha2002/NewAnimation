defcdb038e6269eceb1a1ada06591752417b8fd1
Nika Abashishvili <108979315+abasha2002@users.noreply.github.com>
2024-06-01 12:42:21 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+        heroBeforeAnimation();
+        heroAfterAnimation();
+    }
+
+    function heroBeforeAnimation() {
+        const scrollBefore = document.querySelector(".loop-marquee_item-before");
+        const innerContentBefore = Array.from(scrollBefore.children);
+
+        innerContentBefore.forEach(item => {
+            const duplicateBefore = item.cloneNode(true);
+            duplicateBefore.setAttribute("aria-hidden", true);
+            duplicateBefore.classList.add("duplicated-before-item");
+            scrollBefore.appendChild(duplicateBefore);
+        });
+
+        
+        scrollBefore.setAttribute('data-duplicated', true);
+        console.log('Items duplicated successfully for:', scrollBefore);
+    } 
+
+    function heroAfterAnimation(){
+        const scrollAfter = document.querySelector(".loop-marquee_item-after");
+        const innerContentAfter = Array.from(scrollAfter.children);
+
+        innerContentAfter.forEach(item => {
+            const duplicateAfter = item.cloneNode(true);
+            duplicateAfter.setAttribute("aria-hidden", true);
+            duplicateAfter.classList.add("duplicated-after-item");
+            scrollAfter.appendChild(duplicateAfter);
+        });
+    }
+});
